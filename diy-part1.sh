088243ba1ae45716460a76d0f7a65f8cc312367c
Jack <105053344+meqte@users.noreply.github.com>
2025-12-11 14:09:46 +0800

    Comment out OpenAppFilter feed addition

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,4 @@
 # echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo 'src-git netspeedtest https://github.com/sirpdboy/luci-app-netspeedtest;js' >> feeds.conf.default
-echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter;master' >> feeds.conf.default
+# echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter;master' >> feeds.conf.default
